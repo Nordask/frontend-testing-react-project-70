14bd063542eda8f13bdc8fca573d6287bb4bcfec
Nordask <solomon7dun@gmail.com>
2024-07-28 23:57:35 +0300

    main: first test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 import {
-  render, screen, within, waitFor,
+  render, waitFor,
 } from '@testing-library/react';
 import toDoApp from '@hexlet/react-todo-app-with-backend';
 
-const INITIAL_DATA = {
+const DUMMY_DATA = {
   currentListId: 1,
   lists: [
     { id: 1, name: 'First type tasks', removable: false },
@@ -32,10 +32,20 @@ const INITIAL_DATA = {
 };
 
 describe('test react-todo-app-with-backend', () => {
-  it('should display to-do list', () => {
-    const virtualDom = toDoApp(INITIAL_DATA);
-    render(virtualDom);
+  it('should display to-do list', async () => {
+    const virtualDom = toDoApp(DUMMY_DATA);
+    const { queryAllByText, queryAllByRole } = render(virtualDom);
 
-    console.log('qq1', virtualDom);
+    await waitFor(() => {
+      expect(queryAllByText('Hexlet Todos')).toHaveLength(1);
+      expect(queryAllByText('Lists')).toHaveLength(1);
+      expect(queryAllByText('Tasks')).toHaveLength(1);
+      expect(queryAllByText('First type tasks')).toHaveLength(1);
+      expect(queryAllByText('Second type tasks')).toHaveLength(1);
+      expect(queryAllByRole('button', { name: 'add list' })).toHaveLength(1);
+      expect(queryAllByRole('textbox', { name: 'New list' })).toHaveLength(1);
+      expect(queryAllByRole('button', { name: 'Add' })).toHaveLength(1);
+      expect(queryAllByRole('textbox', { name: 'New task' })).toHaveLength(1);
+    });
   });
 });
